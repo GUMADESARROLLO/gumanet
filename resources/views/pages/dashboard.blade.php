0689a8029ed2178edc03cf9e12b6dcfa170427e7
Ing.Maryan Espinoza <analista.guma@gmail.com>
2021-09-27 11:17:49 -0600

    --amend

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -2,7 +2,7 @@
 @section('title' , $name)
 @section('name_user' , 'Administrador')
 @section('metodosjs')
-  @include('jsViews.js_dashboard')
+@include('jsViews.js_dashboard')
 @endsection
 @section('content')
 <div class="container-fluid">
@@ -22,18 +22,18 @@
                     <div class="form-group">
                         <label for="opcMes" class="text-muted m-0">Filtrar por mes</label>
                         <select class="form-control form-control-sm" id="opcMes">
-                          <?php                        
-                            $mes = date("m");
-                            $meses = array('none','enero','febrero','marzo','abril','mayo','junio','julio','agosto','septiembre','octubre','noviembre','diciembre');
+                            <?php                        
+                                $mes = date("m");
+                                $meses = array('none','enero','febrero','marzo','abril','mayo','junio','julio','agosto','septiembre','octubre','noviembre','diciembre');
 
-                            for ($i= 1; $i <= 12 ; $i++) {
-                              if ($i==$mes) {
-                                echo'<option selected value="'.$i.'">'.$meses[$i].'</option>';
-                              }else {
-                                echo'<option value="'.$i.'">'.$meses[$i].'</option>';
-                              }
-                            }
-                          ?>
+                                for ($i= 1; $i <= 12 ; $i++) {
+                                if ($i==$mes) {
+                                    echo'<option selected value="'.$i.'">'.$meses[$i].'</option>';
+                                }else {
+                                    echo'<option value="'.$i.'">'.$meses[$i].'</option>';
+                                }
+                                }
+                            ?>
                         </select>
                     </div>
                 </div>
@@ -44,12 +44,11 @@
                             <?php
                                 $year = date("Y");
                                 for ($i= 2018; $i <= $year ; $i++) {
-                                  if ($i==$year) {
-                                    echo'<option selected value="'.$i.'">'.$i.'</option>';
-                                  }else {
-                                    echo'<option value="'.$i.'">'.$i.'</option>';
-                                  }
-                                 
+                                    if ($i==$year) {
+                                        echo'<option selected value="'.$i.'">'.$i.'</option>';
+                                    }else {
+                                        echo'<option value="'.$i.'">'.$i.'</option>';
+                                    }
                                 }
                             ?>
                         </select>  
@@ -66,13 +65,20 @@
     <div class="content-graf mb-5">
         <div class="row" id="ct04">
             <div class="graf col-sm-12 mt-3">
-                <div class="container-vms" id="grafVtsMes" style="width: 100%; margin: 0 auto"></div>
+                <div class="container-vms" id="grafVtsDiario" style="width: 100%; margin: 0 auto"></div>
+            </div>
+        </div>
+        
+
+        <div class="row" id="ct04">
+            <div class="graf col-sm-12 mt-3">
+                <div class="container-rvts" id="grafRealVentas" style="width: 100%; margin: 0 auto"></div>
             </div>
         </div>
 
         <div class="row" id="ct04">
             <div class="graf col-sm-12 mt-3">
-                <div class="container-rvts" id="grafRealVentas" style="width: 100%; margin: 0 auto"></div>
+                <div class="container-vms" id="grafVtsMes" style="width: 100%; margin: 0 auto"></div>
             </div>
         </div>
 
@@ -250,9 +256,11 @@
         </div>
         <div class="row center">
             <div class="col-sm-12">
-                <div class="card mt-3 border-0 shadow-sm">
-                  <div class="card-body">
-                    <div class="row">
+                
+                
+                <div class="card mt-3 border-0 shadow-sm">                    
+                  <div class="card-body">                  
+                    <div class="row" id="lbl_id_articulos">
                         <div class="col-sm-4 border-right">
                             <h5 class="card-title" id="title-page-tem"></h5>
                             <p class="text-muted" id="fechaFiltrada"></p>
@@ -268,13 +276,14 @@
                                     <p class="font-weight-bolder" style="font-size: 1.3rem!important" id="MontoReal"></p>
                                 </div>
                                 <div class="col-sm-4" id="cumplMetaContent">
-                                    <p class="text-muted m-0">% Cumpl.</p>
+                                    <p class="text-muted m-0" id="id_detall_unit_bonif">% Cumpl.</p>
                                     <p class="font-weight-bolder text-info" style="font-size: 1.3rem!important" id="cumplMeta"></p>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="row">
+                    
+                    <div class="row" id="id_div_detalles_vendedores">
                         <div class="col-sm-11 mt-3">
                            <div class="input-group">
                                 <div class="input-group-prepend">
@@ -284,7 +293,7 @@
                             </div>
                         </div>
                         <div class="col-sm-1 mt-3">
-                             <div class="input-group">
+                            <div class="input-group">
                                 <select class="custom-select" id="cantRowsDtTemp">
                                     <option value="5" selected>5</option>
                                     <option value="10">10</option>
@@ -295,16 +304,92 @@
                             </div>
                         </div>
                     </div>
-                  </div>
+                    
+                    
+                    <div class="row" >
+                        <div class="col-sm-6 mt-2">
+                            <div class="table-responsive" id="id_div_titulo_Ventas_Rutas">
+                                <div id="cjRutVentasRutas">
+                                    <table class="table table-bordered table-sm" width="100%" id="dtVentaRuta" ></table>
+                                </div>        
+                            </div>
+
+                        </div>
+                        <div class="col-sm-6 mt-2 align-self-center">
+                            <div id="id_grafica_pie_ventas_ruta"></div>
+                        </div>
+                    
+                    </div>
+                </div>
                 </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col-sm-12 mt-2">
+        <br>
+        <div class="row" id="id_detalles_articulos" >
+        <div class="col-12 col-lg-6 col-xl">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row align-items-center gx-0">
+                                    <div class="col">
+                                        <P class="text-muted m-0">PREC. PROM. :</P>
+                                        <p class="font-weight-bolder text-center" style="font-size: 1.3rem!important" id="id_detall_prec_prom"></p>                                    
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-lg-6 col-xl">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row align-items-center gx-0">
+                                <div class="col">
+                                    <p class="text-muted mb-0">COST. PROM. UNIT. :</p>
+                                    <p class="font-weight-bolder text-center" style="font-size: 1.3rem!important" id="id_detall_cost_unit"></p>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-lg-6 col-xl">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row align-items-center gx-0">
+                                <div class="col">
+                                    <p class="text-muted mb-0">CONTRIBUCION:</p>
+                                    <p class="font-weight-bolder text-center" style="font-size: 1.3rem!important" id="id_detall_marg_contrib"></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-lg-6 col-xl">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row align-items-center gx-0">
+                                <div class="col">
+                                    <p class="text-muted mb-0">% MARGEN BRUTO. :</p>
+                                    <p class="font-weight-bolder text-center" style="font-size: 1.3rem!important" id="id_detall_porc_contrib"></p>                                    
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>                                        
+                </div>           
+        <div class="row" >
+
+        <div class="col-sm-12 mt-2" >
+                
+            </div>
+
+            <div class="col-sm-12 mt-2" >
                 <div class="table-responsive">
-                    <div id="cjRutVentas"><table class="table table-bordered table-sm" width="100%" id="dtTotalXRutaVent" ></table></div>        
+                    <div id="cjRutVentas">
+                        <table class="table table-bordered table-sm" width="100%" id="dtTotalXRutaVent" ></table>
+                    </div>        
                 </div>
             </div>
+
             
             <div class="col-sm-12 mt-2">
                 <div class="table-responsive">
@@ -316,7 +401,7 @@
 
             <div class="col-sm-12 mt-2">
                 <div class="table-responsive">
-                   <div id="cjRecu_GumaPharma">
+                    <div id="cjRecu_GumaPharma">
                         <table class="table table-bordered table-sm" width="100%" id="dtRecu_GumaPharma"></table>
                     </div>
                 </div>                    
@@ -338,7 +423,52 @@
                     </div>
                 </div>
             </div>
-        </div>        
+        </div>
+    </div>
+</div>
+<!-- Modal:Detalles de ventas de todos los articulos-->
+<div class="modal fade modal-fullscreen" id="mdDetailsAllItems" tabindex="-1" role="dialog" aria-labelledby="titleModal" aria-hidden="true">
+    <div class="modal-dialog modal-xl" role="document" >
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title font-weight-bolder text-info" id="id_titulo_modal_all_items" ></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body" id="bodyModal">
+            <div class="row" >
+                <div class="col-sm-11 mt-2">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon1"><i data-feather="search"></i></span>
+                        </div>
+                        <input type="text" id="id_txt_all_item"  class="form-control" placeholder="Buscar">
+                    </div>
+                </div>
+                <div class="col-sm-1 mt-2">
+                    <div class="input-group">
+                        <select class="custom-select" id="id_select_all_items" >
+                            <option value="5" selected>5</option>
+                            <option value="10">10</option>
+                            <option value="20">20</option>
+                            <option value="50">50</option>
+                            <option value="-1">Todo</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+                <div class="row">
+                    <div class="col-sm-12 mt-3">
+                        <div class="table-responsive">
+                            <table id="tblAllItems" class="table table-bordered table-sm" width="100%" >
+                                
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 <!-- Modal:Detalle Venta Comparación Meta, Real Cumplimineto -->
@@ -352,7 +482,7 @@
                 </button>
             </div>
             <div class="modal-body" id="bodyModal">
-                <div class="row">
+                <div class="row" id="id_detalles_ventas">
                 <div class="col-sm-2">
                     <p class="text-muted m-0">Meta Units.</p>
                     <p class="font-weight-bolder" style="font-size: 1.3rem!important" id="total_Meta_Unidad"></p>
@@ -379,7 +509,7 @@
                 </div>
             </div>
             <hr>
-            <div class="row">
+            <div class="row" id="id_div_Detalles_venta">
                 <div class="col-sm-11 mt-2">
                    <div class="input-group">
                         <div class="input-group-prepend">
@@ -403,7 +533,12 @@
                 <div class="row">
                     <div class="col-sm-12 mt-3">
                         <div class="table-responsive">
-                            <div id="cjVentas"><table class="table table-bordered table-sm" width="100%" id="dtVentas" ></table></div>
+                            <div id="cjVentas">
+                                <table class="table table-bordered table-sm" width="100%" id="dtVentas" ></table>
+                            </div>
+                            <div id="cjVentasFacturas">
+                                <table class="table table-bordered table-sm" width="100%" id="dtVentasFacturas" ></table>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -411,7 +546,6 @@
         </div>
     </div>
 </div>
-
 <!-- Modal:Detalles de ventas de todos los articulos-->
 <div class="modal fade modal-fullscreen" id="mdDetailsAllItems" tabindex="-1" role="dialog" aria-labelledby="titleModal" aria-hidden="true">
     <div class="modal-dialog modal-xl" role="document" >
