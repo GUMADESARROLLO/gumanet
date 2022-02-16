b9e8832268181cfbc12fa322e091c34ec05b9017
maryan Espinoza <analista.guma@gmail.com>
2022-02-16 14:32:20 -0600

    16/02/2022
    Correciones de errores dentro la seccion de recibos de colector

--- routes/web.php
+++ routes/web.php
@@ -202,4 +202,5 @@ Route::get('getRecibos', 'recibos_controller@getRecibos');
 Route::post('getAttachFile', 'recibos_controller@getAttachFile');
 Route::post('push_recibo', 'recibos_controller@push_recibo');
 Route::post('push_verificado', 'recibos_controller@push_verificado');
+Route::get('print_resumen', 'recibos_controller@print_resumen');
 
