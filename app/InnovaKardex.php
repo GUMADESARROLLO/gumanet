6efd6af9e9b6a5eb8c31f069668e11ad3e40b9f7
Guma Master <analista.guma@gmail.com>
2023-06-29 14:39:51 -0600

    Fix: Calc. correcto de saldos de Kardex Inventario Innova

--- app/InnovaKardex.php
+++ app/InnovaKardex.php
@@ -61,19 +61,22 @@ class InnovaKardex extends Model
                 $json_arrays['header_date_rows'][$i]['UND'] = $r->UND;
                 $json_arrays['header_date_rows'][$i]['USUARIO'] = $RoleUsr->rol->descripcion;
 
-        
+                $dtInit = date('Ymd',strtotime($json_arrays['header_date'][0]));
+                $Articu = $r->ARTICULO;
+
 
                 foreach($json_arrays['header_date'] as $dtFecha => $valor){
 
-                    $rows_in = 'IN01_'.date('Ymd',strtotime($valor));
-                    $rows_out = 'OUT02_'.date('Ymd',strtotime($valor));
+                    $rows_in    = 'IN01_'.date('Ymd',strtotime($valor));
+                    $rows_out   = 'OUT02_'.date('Ymd',strtotime($valor));
                     $rows_stock = 'STOCK03_'.date('Ymd',strtotime($valor));
                     
                     if($valor == 'Ult. Registro.'){
-                        $rows_in = 'IN_TODAY';
-                        $rows_out = 'OUT_TODAY';
+                        $rows_in    = 'IN_TODAY';
+                        $rows_out   = 'OUT_TODAY';
                         $rows_stock = 'STOCK_TODAY';
                     }
+
 
                     if ($r->$rows_stock == '0.00' && $r->$rows_in == '0.00' && $r->$rows_out == '0.00') {
                         $LastStock = $lastStock; 
@@ -84,9 +87,9 @@ class InnovaKardex extends Model
 
 
 
-                    $json_arrays['header_date_rows'][$i][$rows_in] = number_format($r->$rows_in,2)  ;
-                    $json_arrays['header_date_rows'][$i][$rows_out] = number_format($r->$rows_out,2);
-                    $json_arrays['header_date_rows'][$i][$rows_stock] =  number_format($LastStock,2);
+                    $json_arrays['header_date_rows'][$i][$rows_in]      = number_format($r->$rows_in,2)  ;
+                    $json_arrays['header_date_rows'][$i][$rows_out]     = number_format($r->$rows_out,2);
+                    $json_arrays['header_date_rows'][$i][$rows_stock]   =  number_format($LastStock,2);
 
                 
 
