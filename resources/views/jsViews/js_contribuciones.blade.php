fc15a52a76e8106c49da0dcf1f31fc42e0b9ade9
Unimark Analista II <analista2.guma@gmail.com>
2024-08-16 14:33:44 -0600

    fix: [ADD] Total para cantidad, venta, costo y contribucion para cada canal

--- resources/views/jsViews/js_contribuciones.blade.php
+++ resources/views/jsViews/js_contribuciones.blade.php
@@ -4,7 +4,7 @@ $(document).ready(function () {
     inicializaControlFecha();
     $('#id_txt_buscar').on('keyup', function() {   
         var vTable = $('#table_contribucion').DataTable();     
-        vTable.search(this.value).draw();
+        vTable.search(this.value).draw();        
     });
 
     $( "#InputCanales").change(function() {
@@ -143,5 +143,109 @@ $(document).ready(function () {
         });
 
     })
+
+    function calcularTotales() {
+        var table = $('#table_contribucion').DataTable();
+        var Farmacia_Cantidad = Farmacia_Costo = Farmacia_Venta = Farmacia_Contribucion = 0;
+        var Cadena_Farmacia_Cantidad = Cadena_Farmacia_Costo = Cadena_Farmacia_Venta = Cadena_Farmacia_Contribucion = 0;
+        var Mayorista_Cantidad = Mayorista_Costo = Mayorista_Venta = Mayorista_Contribucion = 0;
+        var Cruz_Azul_Cantidad = Cruz_Azul_Costo = Cruz_Azul_Venta = Cruz_Azul_Contribucion = 0;
+        var Institucion_Privada_Cantidad = Institucion_Privada_Costo = Institucion_Privada_Venta = Institucion_Privada_Contribucion = 0;
+        var Institucion_Publica_Cantidad = Institucion_Publica_Costo = Institucion_Publica_Venta = Institucion_Publica_Contribucion = 0;
+        var Total_Cantidad = Total_Costo = Total_Venta = Total_Contribucion = 0;
+
+        table.rows({ search: 'applied' }).every(function() {
+            var data = this.data();
+           
+            // TOTAL DE FARMACIAS
+            Farmacia_Cantidad       += parseFloat(data.FARMACIA_CANTIDAD) || 0;
+            Farmacia_Venta          += parseFloat(data.FARMACIA_VENTA) || 0;
+            Farmacia_Costo          += parseFloat(data.FARMACIA_COSTO) || 0;
+            Farmacia_Contribucion   += parseFloat(data.FARMACIA_CONTRIBUCION) || 0;
+
+            // TOTAL DE CADENA DE FARMACIAS
+            Cadena_Farmacia_Cantidad    += parseFloat(data.CADENA_FARMACIA_CANTIDAD) || 0;
+            Cadena_Farmacia_Venta       += parseFloat(data.CADENA_FARMACIA_VENTA) || 0;
+            Cadena_Farmacia_Costo       += parseFloat(data.CADENA_FARMACIA_COSTO) || 0;
+            Cadena_Farmacia_Contribucion+= parseFloat(data.CADENA_FARMACIA_CONTRIBUCION) || 0;
+
+            // TOTAL DE MAYORISTAS
+            Mayorista_Cantidad      += parseFloat(data.MAYORISTA_CANTIDAD) || 0;
+            Mayorista_Venta         += parseFloat(data.MAYORISTA_VENTA) || 0;
+            Mayorista_Costo         += parseFloat(data.MAYORISTA_COSTO) || 0;
+            Mayorista_Contribucion  += parseFloat(data.MAYORISTA_CONTRIBUCION) || 0;
+
+            // TOTAL DE CRUZ AZUL
+            Cruz_Azul_Cantidad      += parseFloat(data.CRUZ_AZUL_CANTIDAD) || 0;
+            Cruz_Azul_Venta         += parseFloat(data.CRUZ_AZUL_VENTA) || 0;
+            Cruz_Azul_Costo         += parseFloat(data.CRUZ_AZUL_COSTO) || 0;
+            Cruz_Azul_Contribucion  += parseFloat(data.CRUZ_AZUL_CONTRIBUCION) || 0;
+
+            // TOTAL INTITUCION PRIVADA
+            Institucion_Privada_Cantidad    += parseFloat(data.INSTITUCION_PRIVADA_CANTIDAD) || 0;
+            Institucion_Privada_Venta       += parseFloat(data.INSTITUCION_PRIVADA_VENTA) || 0;
+            Institucion_Privada_Costo       += parseFloat(data.INSTITUCION_PRIVADA_COSTO) || 0;
+            Institucion_Privada_Contribucion+= parseFloat(data.INSTITUCION_PRIVADA_CONTRIBUCION) || 0;
+
+            // TOTAL INTITUCION PUBLICA
+            Institucion_Publica_Cantidad    += parseFloat(data.INSTITUCION_PUBLICA_CANTIDAD) || 0;
+            Institucion_Publica_Venta       += parseFloat(data.INSTITUCION_PUBLICA_VENTA) || 0;
+            Institucion_Publica_Costo       += parseFloat(data.INSTITUCION_PUBLICA_COSTO) || 0;
+            Institucion_Publica_Contribucion+= parseFloat(data.INSTITUCION_PUBLICA_CONTRIBUCION) || 0;
+
+            // TOTAL DE TOTALES
+            Total_Cantidad      += parseFloat(data.TOTAL_VENTAS_PACK) || 0;
+            Total_Venta         += parseFloat(data.TOTAL_VENTAS_C$) || 0;
+            Total_Costo         += parseFloat(data.TOTAL_COSTOS_C$) || 0;
+            Total_Contribucion  += parseFloat(data.TOTAL_CONTRIBUCION_C$) || 0;
+        });
+
+        // TOTAL DE FARMACIAS
+        $('#Farmacia_Cantidad').html(numeral(Farmacia_Cantidad).format('0,0'));
+        $('#Farmacia_Venta').html('C$ '+numeral(Farmacia_Venta).format('0,0.00'));
+        $('#Farmacia_Costo').html('C$ '+numeral(Farmacia_Costo).format('0,0.00'));
+        $('#Farmacia_Contribucion').html('C$ '+numeral(Farmacia_Contribucion).format('0,0.00'));
+
+        // TOTAL DE CADENA DE FARMACIAS
+        $('#Cadena_Farmacia_Cantidad').html(numeral(Cadena_Farmacia_Cantidad).format('0,0'));
+        $('#Cadena_Farmacia_Venta').html('C$ '+numeral(Cadena_Farmacia_Venta).format('0,0.00'));
+        $('#Cadena_Farmacia_Costo').html('C$ '+numeral(Cadena_Farmacia_Costo).format('0,0.00'));
+        $('#Cadena_Farmacia_Contribucion').html('C$ '+numeral(Cadena_Farmacia_Contribucion).format('0,0.00'));
+
+        // TOTAL DE MAYORISTAS
+        $('#Mayorista_Cantidad').html(numeral(Mayorista_Cantidad).format('0,0'));
+        $('#Mayorista_Venta').html('C$ '+numeral(Mayorista_Venta).format('0,0.00'));
+        $('#Mayorista_Costo').html('C$ '+numeral(Mayorista_Costo).format('0,0.00'));
+        $('#Mayorista_Contribucion').html('C$ '+numeral(Mayorista_Contribucion).format('0,0.00'));
+
+        // TOTAL INTITUCION PRIVADA
+        $('#Institucion_Privada_Cantidad').html(numeral(Institucion_Privada_Cantidad).format('0,0'));
+        $('#Institucion_Privada_Venta').html('C$ '+numeral(Institucion_Privada_Venta).format('0,0.00'));
+        $('#Institucion_Privada_Costo').html('C$ '+numeral(Institucion_Privada_Costo).format('0,0.00'));
+        $('#Institucion_Privada_Contribucion').html('C$ '+numeral(Institucion_Privada_Contribucion).format('0,0.00'));
+        
+        // TOTAL DE CRUZ AZUL
+        $('#Cruz_Azul_Cantidad').html(numeral(Cruz_Azul_Cantidad).format('0,0'));
+        $('#Cruz_Azul_Venta').html('C$ '+numeral(Cruz_Azul_Venta).format('0,0.00'));
+        $('#Cruz_Azul_Costo').html('C$ '+numeral(Cruz_Azul_Costo).format('0,0.00'));
+        $('#Cruz_Azul_Contribucion').html('C$ '+numeral(Cruz_Azul_Contribucion).format('0,0.00'));
+
+        // TOTAL INTITUCION PUBLICA
+        $('#Institucion_Publica_Cantidad').html(numeral(Institucion_Publica_Cantidad).format('0,0'));
+        $('#Institucion_Publica_Venta').html('C$ '+numeral(Institucion_Publica_Venta).format('0,0.00'));
+        $('#Institucion_Publica_Costo').html('C$ '+numeral(Institucion_Publica_Costo).format('0,0.00'));
+        $('#Institucion_Publica_Contribucion').html('C$ '+numeral(Institucion_Publica_Contribucion).format('0,0.00'));
+
+        // TOTAL DE TOTALES
+        $('#Total_Cantidad').html(numeral(Total_Cantidad).format('0,0'));
+        $('#Total_Venta').html('C$ '+numeral(Total_Venta).format('0,0.00'));
+        $('#Total_Costo').html('C$ '+numeral(Total_Costo).format('0,0.00'));
+        $('#Total_Contribucion').html('C$ '+numeral(Total_Contribucion).format('0,0.00'));
+    }
+
+    $('#table_contribucion').DataTable().on('draw', function() {
+        calcularTotales();
+    });
+
 });
 </script>
