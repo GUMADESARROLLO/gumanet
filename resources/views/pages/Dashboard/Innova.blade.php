91618fcf73c8417488d858459dde339427aa8d92
Guma Desarrollo <ANALISTA.GUMA@GMAIL.COM>
2025-06-26 09:20:50 -0600

    ✅ [ADD] New Dashboard Innova

---
+++ resources/views/pages/Dashboard/Innova.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.main')
+
+
+@section('title' , $name)
+    @section('name_user' , 'Administrador')
+@section('metodosjs')
+    @include('Pages.Dashboard.js_dashboard_innova')
+@endsection
+
+@section('content')
+
+@endsection
