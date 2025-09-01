721c64f10018587841da4c0739e689db8311bf22
Guma Desarrollo <ANALISTA.GUMA@GMAIL.COM>
2025-09-01 14:01:43 -0600

    🐛 [FIX] Name Xls Recibos

--- app/Http/Controllers/recibos_controller.php
+++ app/Http/Controllers/recibos_controller.php
@@ -372,7 +372,7 @@ class recibos_controller extends Controller {
 
 
         header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-        header('Content-Disposition: attachment;filename="GastosOperativos.xlsx"');
+        header('Content-Disposition: attachment;filename="Recibos.xlsx"');
         header('Cache-Control: max-age=0');
         $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
         $objWriter->save('php://output');
