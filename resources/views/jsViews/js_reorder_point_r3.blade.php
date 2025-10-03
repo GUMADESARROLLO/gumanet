f84ada1290631699bb519d1e24394c178e31ba31
Guma Desarrollo <ANALISTA.GUMA@GMAIL.COM>
2025-10-03 13:28:08 -0600

    ✅ [ADD] FECHA_VENCIMIENTO to reorder

--- resources/views/jsViews/js_reorder_point_r3.blade.php
+++ resources/views/jsViews/js_reorder_point_r3.blade.php
@@ -31,7 +31,8 @@
         { data: "DESCONTINUADO", title: "DESCONTINUADO", class: "text-center"},
         { data: "FECHA_VENCE_LOTE", title: "VENC. LOTE", class: "text-left" },   
         { data: "CANTIDAD_INGRESADA", title: "ULT. CANT. INGRESADA", class: "text-right", render: $.fn.dataTable.render.number(',', '.', 2) },
-        { data: "CANT_VENCE_LOTE", title: "CANT. VENCE LOTE", class: "text-right", render: $.fn.dataTable.render.number(',', '.', 2) },           
+        { data: "CANT_VENCE_LOTE", title: "CANT. VENCE LOTE", class: "text-right", render: $.fn.dataTable.render.number(',', '.', 2) },     
+        { data: "FECHA_VENCIMIENTO", title: "FECHA VENC.", class: "text-left" },      
     ];
 
     let topStart_custom = document.createElement('div');
