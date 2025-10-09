0a7be1b4c6731e31db322973dd681e0ad5cfc942
Guma Desarrollo <ANALISTA.GUMA@GMAIL.COM>
2025-10-09 16:07:41 -0600

    ✅ [ADD] Models Produccion Articulo

--- app/ArticulosTransito.php
+++ app/ArticulosTransito.php
@@ -20,6 +20,12 @@ class ArticulosTransito extends Model
     protected $primaryKey = 'Id_transito';
     protected $keyType    = 'string';
 
+
+    public function getArticulo()
+    {
+        return $this->belongsTo(Articulo::class, 'Articulo', 'ARTICULO');
+    }
+
     protected $fillable = [
         'Articulo',
         'Descripcion',
@@ -102,7 +108,8 @@ class ArticulosTransito extends Model
             $Array[$k] = [
                 'ID'                => $v['Id_transito'],
                 'ARTICULO'          => $v['Articulo'],
-                'DESCRIPCION'       => strtoupper($v['Descripcion']),
+                //'DESCRIPCION'       => strtoupper($v['Descripcion']),
+                'DESCRIPCION'       => strtoupper($v->getArticulo->DESCRIPCION) ?? 'N/D',
                 'FECHA_ESTIMADA'    => ($v['fecha_estimada']== null) ? 'N/D' : \Date::parse($v['fecha_estimada'])->format('D, M d, Y') ,
                 'FECHA_PEDIDO'      => ($v['fecha_pedido']== null) ? 'N/D' : \Date::parse($v['fecha_pedido'])->format('D, M d, Y') ,
                 'PEDIDO'            => number_format($v['cantidad_pedido'], 0),
